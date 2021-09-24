e0062562b2d88630dbdaa9d2775f78defe4e5853
clopezr9 <clopezr9@eafit.edu.co>
2021-09-24 16:22:16 -0500

    no se

--- Client.py
+++ Client.py
@@ -4,22 +4,24 @@ import sys
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-IP_address = "18.233.110.201"
-Port = 3550
-server.connect((IP_address, Port))
+def main(argv):
+    ip_address = argv[1]
+    port = argv[2]
+    server.connect((ip_address, port))
 
-while True:
-	sockets_list = [sys.stdin, server]
-	read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
+    while True:
+    	sockets_list = [sys.stdin, server]
+    	read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
 
-	for socks in read_sockets:
-            if socks == server:
-                message = socks.recv(2048)
-                print (message.decode())
-            else:
-                message = sys.stdin.readline()
-                server.send(message.encode())
-                sys.stdout.write("<You>")
-                sys.stdout.write(message)
-                sys.stdout.flush()
+    	for socks in read_sockets:
+                if socks == server:
+                    message = socks.recv(2048)
+                    print (message.decode())    
+                else:
+                    message = sys.stdin.readline()
+                    server.send(message.encode())
+                    sys.stdout.write(message)
+                    sys.stdout.flush()
+
+main(sys.argv)
 server.close()
--- Server.py
+++ Server.py
@@ -3,41 +3,49 @@ import select
 import sys
 from  _thread import *
 
-#idk
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-IP_address = "0.0.0.0"
+def main(argv):
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-Port = 3550
+    ip_address = argv[1]
+    port = argv[2]
 
-server.bind((IP_address, Port))
+    server.bind((ip_address, port))
+    server.listen(100)
 
-server.listen(100)
 
 list_of_clients = []
 
 def clientthread(conn, addr):
         #sends message
-	conn.send(b'You are part of this group chat!')
+	conn.send(b'Welcome to NASAs data storage')
 
 	while True:
             try:
                 message = conn.recv(2048).decode()
 
-                if message:
-                    print("este es addr[0]" + str(addr[0]))
-                    print("<" + str(addr[0]) + "> " + message)
+                if message: 
+                    #Conection to DB and save records
+                    #Message structure I/key/value no spaces or R/key/num
+                    msg = message.split('/')
+                    if  msg.size() == 3:
+                        if msg[0] == "I":
+                            #how to save a record in the DB
+                            message_to_send = "<" + str(addr[0]) + "> " + 'address saved a record'
+                            send_to_clients(message_to_send, conn)
+                        elif msg[0] =="R":
+                            #how to read records from the DB
+                            message_to_send = "<" + str(addr[0]) + "> " + 'address had read' + msg[2] + 'records'
+                            send_to_clients(message_to_send, conn)
                     
-                    message_to_send = "<" + str(addr[0]) + "> " + message
-                    sendToClients(message_to_send, conn)
                 else:
                     remove(conn)
             except:
                 continue
 
-#sends to all clients
-def sendToClients(message, connection):
+#sends to all cleints that register have been saved
+def send_to_clients(message, connection):
     for clients in list_of_clients:
         print(clients)
         if clients!=connection:
