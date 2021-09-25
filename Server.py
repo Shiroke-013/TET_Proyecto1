fcb321d36ed31439e57581b36977eb45b88cd88c
Shiroke-013 <Shiroke4321@gmail.com>
2021-09-25 15:49:16 -0500

    Still working

--- Server.py
+++ Server.py
@@ -39,7 +39,7 @@ def main(argv):
 	    print (addr[0] + " connected")
 
 	    # creates a thread for every client
-	    start_new_thread(client_thread,(conn,addr, cur, connection))	
+	    start_new_thread(client_thread,(conn,addr, cur, connection, host, user, psswd))	
 
     server.close()
 
