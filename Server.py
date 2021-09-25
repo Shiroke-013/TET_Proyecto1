f7009fd7bfc084e2563094596702d3176d39c453
clopezr9 <clopezr9@eafit.edu.co>
2021-09-25 10:01:19 -0500

    Select ready?

--- Server.py
+++ Server.py
@@ -1,6 +1,7 @@
 import socket
 import select
 import sys
+import pymysql
 from  _thread import *
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -30,24 +31,31 @@ def main(argv):
 
 
 def client_thread(conn, addr):
+    cur = start_connection() 
     #sends message
-	conn.send(b'Welcome to NASAs data storage')
+    conn.send(b'Welcome to NASAs data storage')
 
-	while True:
+    while True:
             try:
                 message = conn.recv(2048).decode()
 
                 if message: 
                     #Conection to DB and save records
-                    #Message structure I/key/value no spaces or R/key/num
+                    #Message structure I/key/value no spaces or S/key/num
                     msg = message.split('/')
                     if  msg.size() == 3:
                         if msg[0] == "I":
                             #how to save a record in the DB
                             message_to_send = "<" + str(addr[0]) + "> " + 'address saved a record'
                             send_to_clients(message_to_send, conn)
-                        elif msg[0] =="R":
-                            #how to read records from the DB
+
+                        elif msg[0] =="S":
+                            #how to select records from the DB
+                            sel = "SELECT {} FROM {}  WHERE {}".format(msg[2], db_name, msg[1])
+                            data = cur.execute(sel)
+                            for rec in data:
+                                print (rec[0] + "," + rec[1])
+                            
                             message_to_send = "<" + str(addr[0]) + "> " + 'address had read' + msg[2] + 'records'
                             send_to_clients(message_to_send, conn)
                     
@@ -55,6 +63,7 @@ def client_thread(conn, addr):
                     remove(conn)
             except:
                 continue
+
 
 #sends to all cleints that register have been saved
 def send_to_clients(message, connection):
@@ -66,6 +75,7 @@ def send_to_clients(message, connection):
             except:
                 clients.close()
                 remove(clients)
+
 
 #remove client if desconected
 def remove(connection):
